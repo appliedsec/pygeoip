6e94f20b9fce0a0b1e50d2f118c8a6eb942d6d77
William Tisäter <william@defunct.cc>
2013-07-16 03:03:53 +0200

    Always run build.sh from repository root

--- build.sh
+++ build.sh
@@ -3,29 +3,31 @@
 function warning { echo "Warning: $1"; }
 function error { echo "Error: $1" 1>&2; exit 1; }
 
-if [ -z "$(virtualenv --version)" ]; then
-    error "Missing virtualenv binary"
-fi
+pushd $(dirname $0)
+    if [ -z "$(virtualenv --version)" ]; then
+        error "Missing virtualenv binary"
+    fi
 
-if [ ! -d venv ]; then
-	virtualenv venv || error "virtualenv failed"
-	venv/bin/pip install tox nose epydoc || error "pip failed"
-fi
+    if [ ! -d venv ]; then
+        virtualenv venv || error "virtualenv failed"
+        venv/bin/pip install tox nose epydoc || error "pip failed"
+    fi
 
-if [ ! -d tests/data ]; then
-    pushd tests
-        wget http://www.defunct.cc/maxmind-geoip-samples.tar.gz
-        tar -zxvf maxmind-geoip-samples.tar.gz
-        unlink maxmind-geoip-samples.tar.gz
-    popd
-fi
+    if [ ! -d tests/data ]; then
+        pushd tests
+            wget http://www.defunct.cc/maxmind-geoip-samples.tar.gz
+            tar -zxvf maxmind-geoip-samples.tar.gz
+            unlink maxmind-geoip-samples.tar.gz
+        popd
+    fi
 
-venv/bin/tox || error "tox failed"
-venv/bin/epydoc --config=epydoc.ini --no-private || error "epydoc failed"
+    venv/bin/tox || error "tox failed"
+    venv/bin/epydoc --config=epydoc.ini --no-private || error "epydoc failed"
 
-if [ -z "$(pandoc --version)" ]; then
-    warning "Skipping Markdown to reStructuredText translation"
-else
-    pandoc -f markdown -t rst -o README.rst README.md || error "pandoc failed"
-fi
+    if [ -z "$(pandoc --version)" ]; then
+        warning "Skipping Markdown to reStructuredText translation"
+    else
+        pandoc -f markdown -t rst -o README.rst README.md || error "pandoc failed"
+    fi
+popd
 
