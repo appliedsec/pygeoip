2220c964e7ab03ad15ef7bef942c68038daaa19d
William Tisäter <william@defunct.cc>
2013-11-14 11:09:55 +0100

    Drop epydoc and setup initial Sphinx framework

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to pygeoip&#8217;s documentation!"],objects:{},titleterms:{pygeoip:0,document:0,welcom:0,indic:0,tabl:0}})
