7501c9327fc80e78be3f44585395e30141d8c749
William Tisäter <william@defunct.cc>
2014-10-27 21:47:21 +0100

    Bump version

--- pygeoip/__init__.py
+++ pygeoip/__init__.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/lgpl.txt>.
 """
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 import os
 import socket
